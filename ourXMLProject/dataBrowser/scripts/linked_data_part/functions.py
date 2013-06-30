e77bdb459765d460ff41a6316b6812af84c08617
troublemake <troublemakershome@web.de>
2013-06-30 21:44:56 +0200

    added second query

--- ourXMLProject/dataBrowser/scripts/linked_data_part/functions.py
+++ ourXMLProject/dataBrowser/scripts/linked_data_part/functions.py
@@ -14,6 +14,13 @@ from django.utils.html import *
 # lxml
 from lxml import etree
 
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
 def itIsSPARQL(clientRequest, contentType, response):
     print >>sys.stderr,clientRequest.request.POST['uri']
     uri = clientRequest.request.POST['uri']
@@ -23,14 +30,43 @@ def itIsSPARQL(clientRequest, contentType, response):
     sparql = SPARQLWrapper("http://"+uri2[0]+"/sparql")
     sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label ?comment ?sameAs
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+    SELECT ?label ?comment ?sameAs ?primaryTopic ?birthPlace
     WHERE {
-	 <"""+clientRequest.request.POST['uri']+"""> rdfs:label ?label.
-	<"""+clientRequest.request.POST['uri']+""">  rdfs:comment ?comment.
- 	<"""+clientRequest.request.POST['uri']+""">  owl:sameAs ?sameAs}""")
+	<"""+clientRequest.request.POST['uri']+"""> rdfs:label ?label.
+ 	<"""+clientRequest.request.POST['uri']+"""> owl:sameAs ?sameAs.
+	<"""+clientRequest.request.POST['uri']+"""> rdfs:comment ?comment.
+	<"""+clientRequest.request.POST['uri']+"""> foaf:isPrimaryTopicOf ?primaryTopic.	
+	<"""+clientRequest.request.POST['uri']+"""> dbpedia-owl:birthPlace ?birthPlace.
+}""")
     sparql.setReturnFormat(XML)
     response = sparql.query()
-    return response.convert().toxml()
+    dom = response.convert()
+    bindingElements = dom.getElementsByTagName("binding")
+    birthPlaceURL =""
+    for elem in bindingElements:
+#	if elem.hasAttribute("name")
+	if elem.getAttribute("name") == "birthPlace":
+	       	birthPlaceURL = elem.getElementsByTagName("uri")[0].childNodes[0].data
+		break
+    sparql = SPARQLWrapper("http://"+uri2[0]+"/sparql")
+    sparql.setQuery("""
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?label ?comment ?sameAs ?primaryTopic ?birthPlace
+    WHERE {
+	<"""+birthPlaceURL+"""> rdfs:comment ?comment.
+    }""")
+    sparql.setReturnFormat(XML)
+    response2 = sparql.query()
+    xmlString = dom.toprettyxml()
+  #  test = etree.XML(xmlString)
+   
+    return dom.toprettyxml()
+
 #render(clientRequest.request, 'databrowser/oai_xml_part/results.html', {'searchtext' : "XML:STANDARD" + contentType}
     #results.print_results()
+#PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+#    PREFIX  owl: <http://www.w3.org/2002/07/owl> 
 
