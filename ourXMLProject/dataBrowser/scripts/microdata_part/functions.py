3b418a9c1b2f417b147c1f967235e7e531246d87
florian <florian.otto@fu-berlin.de>
2013-07-03 13:03:42 +0200

    microdata

--- ourXMLProject/dataBrowser/scripts/microdata_part/functions.py
+++ ourXMLProject/dataBrowser/scripts/microdata_part/functions.py
@@ -1,21 +1,21 @@
-from django.http import HttpRespnse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import *
 from django.template.loader import *
-from django shortcuts import render
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from urllib import *
 from dataBrowser.models.microdata_part.models import *
 from xml.dom.minidom import *
 from django.utils.html import *
 from dataBrowser.scripts.basic.functions import *
-from lxml iport etree
+from lxml import etree
 
 import rdflib
-import rdflib_microdata
+import microdata
 
 def itIsMicrodata(clientRequest):
-	uri = clientRequest.POST['uri']
-	g = rdflib.Graph()
-	g.parse(uri, format="microdata")
-	return g.serialize()
+    uri = clientRequest.POST['uri']
+    g = rdflib.Graph()
+    g.parse(uri, format="microdata")
+    return g.serialize()
 	
